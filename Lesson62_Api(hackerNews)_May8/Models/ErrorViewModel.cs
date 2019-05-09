171426d4343b2e5f85a2a2ea089bb922f7e657f2
Henny Yoffe <hennyy1149@gmail.com>
2019-05-08 20:56:23 -0400

    Add project files.

---
+++ Lesson62_Api(hackerNews)_May8/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lesson62_Api_hackerNews__May8.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
